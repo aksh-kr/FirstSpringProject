a7895a4322739bbdb9afeca428d353793f1739dc
Akash Kumar <kr.akash149@gmail.com>
2024-03-20 09:59:53 +0530

    Added RetroFit Support

--- src/main/java/com/example/FirstSpringProject/HelloController.java
+++ src/main/java/com/example/FirstSpringProject/HelloController.java
@@ -1,7 +1,14 @@
 package com.example.FirstSpringProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
 
 @RestController
 public class HelloController {
@@ -18,6 +25,20 @@ public class HelloController {
     @GetMapping("/test")
     public String getTest() {
         return "testing ... 1 2 3";
+    }
+
+    @GetMapping("/todo/{id}")
+    public String getRetroFit(@PathVariable String id) throws IOException {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ToDoService toDoService = retrofit.create(ToDoService.class);
+
+        ToDo toDo = toDoService.getToDoById(id).execute().body();
+
+        return toDo.toString();
     }
 
 }
